353c47c1f1a5e4ea770581d29ef7abca4f0e35e0
t-kashima <takumi.kashima@nanapi.co.jp>
2016-01-06 11:20:37 +0900

    arrange the variable names

--- app/src/main/java/com/unuuu/sample/MainActivity.java
+++ app/src/main/java/com/unuuu/sample/MainActivity.java
@@ -11,7 +11,6 @@ import android.media.MediaRecorder;
 import android.os.Bundle;
 import android.text.format.DateUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
@@ -27,6 +26,7 @@ import java.nio.ByteBuffer;
 import java.nio.ShortBuffer;
 import java.util.List;
 
+import static org.bytedeco.javacpp.avcodec.AV_CODEC_ID_AAC;
 import static org.bytedeco.javacpp.avcodec.AV_CODEC_ID_H264;
 
 public class MainActivity extends Activity {
@@ -36,10 +36,10 @@ public class MainActivity extends Activity {
     private static final String OUTPUT_PATH = "/mnt/sdcard/stream.mp4";
     private static final int SAMPLE_AUDIO_RATE = 44100;
     private static final int FRAME_RATE = 30;
-    /* The number of seconds in the continuous record loop (or 0 to disable loop). */
-    private static final int RECORD_LENGTH = 10;
+    private static final int MAX_RECORD_SECONDS = 5;
     private static final int PREVIEW_BASE_WIDTH = 320;
     private static final int PREVIEW_BASE_HEIGHT = 240;
+    private static final String VIDEO_FORMAT = "mp4";
 
     long mStartTime = 0;
     boolean mIsRecording = false;
@@ -70,7 +70,6 @@ public class MainActivity extends Activity {
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         setContentView(R.layout.activity_main);
 
-        /* add control button: start and stop */
         mRecordButton = (Button) findViewById(R.id.activity_main_button_record);
         mRecordButton.setText("Start");
         mRecordButton.setOnClickListener(new OnClickListener() {
@@ -80,7 +79,6 @@ public class MainActivity extends Activity {
                     startRecording();
                     mRecordButton.setText("Stop");
                 } else {
-                    // This will trigger the audio recording loop to stop and then set isRecorderStart = false;
                     stopRecording();
                     mRecordButton.setText("Start");
                 }
@@ -95,47 +93,33 @@ public class MainActivity extends Activity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-
         mIsRecording = false;
     }
 
-    //---------------------------------------
-    // initialize ffmpeg_recorder
-    //---------------------------------------
-    private void initRecorder() {
-
-        Log.w(LOG_TAG,"init recorder");
-
-        Frame yuvImage = null;
-        if (RECORD_LENGTH > 0) {
-            mImagesIndex = 0;
-            mImageFrames = new Frame[RECORD_LENGTH * FRAME_RATE];
-            mTimestamps = new long[mImageFrames.length];
-            for (int i = 0; i < mImageFrames.length; i++) {
-                mImageFrames[i] = new Frame(mPreviewWidth, mPreviewHeight, Frame.DEPTH_UBYTE, 2);
-                mTimestamps[i] = -1;
-            }
-        } else if (yuvImage == null) {
-            yuvImage = new Frame(mPreviewWidth, mPreviewHeight, Frame.DEPTH_UBYTE, 2);
-            Log.i(LOG_TAG, "create yuvImage");
+    /**
+     * 録画を開始する
+     */
+    public void startRecording() {
+        mImagesIndex = 0;
+        mImageFrames = new Frame[MAX_RECORD_SECONDS * FRAME_RATE];
+        mTimestamps = new long[mImageFrames.length];
+        for (int i = 0; i < mImageFrames.length; i++) {
+            mImageFrames[i] = new Frame(mPreviewWidth, mPreviewHeight, Frame.DEPTH_UBYTE, 2);
+            mTimestamps[i] = -1;
         }
 
-        Log.i(LOG_TAG, "output: " + OUTPUT_PATH);
+        Log.d(LOG_TAG, "output: " + OUTPUT_PATH);
 
         mRecorder = new FFmpegFrameRecorder(OUTPUT_PATH, mPreviewWidth, mPreviewHeight, 1);
-        mRecorder.setFormat("mp4");
+        mRecorder.setFormat(VIDEO_FORMAT);
         mRecorder.setVideoCodec(AV_CODEC_ID_H264);
-        mRecorder.setSampleRate(SAMPLE_AUDIO_RATE);
         mRecorder.setFrameRate(FRAME_RATE);
+        mRecorder.setAudioCodec(AV_CODEC_ID_AAC);
+        mRecorder.setSampleRate(SAMPLE_AUDIO_RATE);
 
         mAudioRecordRunnable = new AudioRecordRunnable();
         mAudioThread = new Thread(mAudioRecordRunnable);
         mRunAudioThread = true;
-    }
-
-    public void startRecording() {
-
-        initRecorder();
 
         try {
             mRecorder.start();
@@ -143,62 +127,62 @@ public class MainActivity extends Activity {
             mIsRecording = true;
             mAudioThread.start();
         } catch (FFmpegFrameRecorder.Exception e) {
-            e.printStackTrace();
+            Log.e(LOG_TAG, e.getMessage());
         }
     }
 
+    /**
+     * 録画を止める
+     */
     public void stopRecording() {
         mRunAudioThread = false;
         try {
             mAudioThread.join();
         } catch (InterruptedException e) {
-            e.printStackTrace();
+            Log.e(LOG_TAG, e.toString());
         }
         mAudioRecordRunnable = null;
         mAudioThread = null;
 
         if (mRecorder != null && mIsRecording) {
-            if (RECORD_LENGTH > 0) {
-                Log.v(LOG_TAG,"Writing frames");
-                try {
-                    int firstIndex = mImagesIndex % mSamples.length;
-                    int lastIndex = (mImagesIndex - 1) % mImageFrames.length;
-                    if (mImagesIndex <= mImageFrames.length) {
-                        firstIndex = 0;
-                        lastIndex = mImagesIndex - 1;
-                    }
-                    if ((mStartTime = mTimestamps[lastIndex] - RECORD_LENGTH * 1000000L) < 0) {
-                        mStartTime = 0;
-                    }
-                    if (lastIndex < firstIndex) {
-                        lastIndex += mImageFrames.length;
-                    }
-                    for (int i = firstIndex; i <= lastIndex; i++) {
-                        long t = mTimestamps[i % mTimestamps.length] - mStartTime;
-                        if (t >= 0) {
-                            if (t > mRecorder.getTimestamp()) {
-                                mRecorder.setTimestamp(t);
-                            }
-                            mRecorder.record(mImageFrames[i % mImageFrames.length]);
-                        }
-                    }
-
-                    firstIndex = mSamplesIndex % mSamples.length;
-                    lastIndex = (mSamplesIndex - 1) % mSamples.length;
-                    if (mSamplesIndex <= mSamples.length) {
-                        firstIndex = 0;
-                        lastIndex = mSamplesIndex - 1;
-                    }
-                    if (lastIndex < firstIndex) {
-                        lastIndex += mSamples.length;
-                    }
-                    for (int i = firstIndex; i <= lastIndex; i++) {
-                        mRecorder.recordSamples(mSamples[i % mSamples.length]);
-                    }
-                } catch (FFmpegFrameRecorder.Exception e) {
-                    Log.v(LOG_TAG,e.getMessage());
-                    e.printStackTrace();
+            Log.v(LOG_TAG,"Writing frames");
+            try {
+                int firstIndex = mImagesIndex % mSamples.length;
+                int lastIndex = (mImagesIndex - 1) % mImageFrames.length;
+                if (mImagesIndex <= mImageFrames.length) {
+                    firstIndex = 0;
+                    lastIndex = mImagesIndex - 1;
                 }
+                if ((mStartTime = mTimestamps[lastIndex] - MAX_RECORD_SECONDS * 1000000L) < 0) {
+                    mStartTime = 0;
+                }
+                if (lastIndex < firstIndex) {
+                    lastIndex += mImageFrames.length;
+                }
+                for (int i = firstIndex; i <= lastIndex; i++) {
+                    long t = mTimestamps[i % mTimestamps.length] - mStartTime;
+                    if (t >= 0) {
+                        if (t > mRecorder.getTimestamp()) {
+                            mRecorder.setTimestamp(t);
+                        }
+                        mRecorder.record(mImageFrames[i % mImageFrames.length]);
+                    }
+                }
+
+                firstIndex = mSamplesIndex % mSamples.length;
+                lastIndex = (mSamplesIndex - 1) % mSamples.length;
+                if (mSamplesIndex <= mSamples.length) {
+                    firstIndex = 0;
+                    lastIndex = mSamplesIndex - 1;
+                }
+                if (lastIndex < firstIndex) {
+                    lastIndex += mSamples.length;
+                }
+                for (int i = firstIndex; i <= lastIndex; i++) {
+                    mRecorder.recordSamples(mSamples[i % mSamples.length]);
+                }
+            } catch (FFmpegFrameRecorder.Exception e) {
+                Log.e(LOG_TAG, e.getMessage());
             }
 
             mIsRecording = false;
@@ -207,100 +191,63 @@ public class MainActivity extends Activity {
                 mRecorder.stop();
                 mRecorder.release();
             } catch (FFmpegFrameRecorder.Exception e) {
-                e.printStackTrace();
+                Log.e(LOG_TAG, e.getMessage());
             }
             mRecorder = null;
         }
     }
 
-    @Override
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        if (keyCode == KeyEvent.KEYCODE_BACK) {
-            if (mIsRecording) {
-                stopRecording();
-            }
-
-            finish();
-
-            return true;
-        }
-
-        return super.onKeyDown(keyCode, event);
-    }
-
-
-    //---------------------------------------------
-    // audio thread, gets and encodes audio data
-    //---------------------------------------------
+    /**
+     * 録画中に録音するクラス
+     */
     class AudioRecordRunnable implements Runnable {
-
         @Override
         public void run() {
             android.os.Process.setThreadPriority(android.os.Process.THREAD_PRIORITY_URGENT_AUDIO);
 
-            // Audio
-            int bufferSize;
-            ShortBuffer audioData;
-            int bufferReadResult;
+            int bufferSize = AudioRecord.getMinBufferSize(SAMPLE_AUDIO_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
+            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_AUDIO_RATE, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
 
-            bufferSize = AudioRecord.getMinBufferSize(SAMPLE_AUDIO_RATE,
-                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT);
-            mAudioRecord = new AudioRecord(MediaRecorder.AudioSource.MIC, SAMPLE_AUDIO_RATE,
-                    AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT, bufferSize);
-
-            if (RECORD_LENGTH > 0) {
-                mSamplesIndex = 0;
-                mSamples = new ShortBuffer[RECORD_LENGTH * SAMPLE_AUDIO_RATE * 2 / bufferSize + 1];
-                for (int i = 0; i < mSamples.length; i++) {
-                    mSamples[i] = ShortBuffer.allocate(bufferSize);
-                }
-            } else {
-                audioData = ShortBuffer.allocate(bufferSize);
+            mSamplesIndex = 0;
+            mSamples = new ShortBuffer[MAX_RECORD_SECONDS * SAMPLE_AUDIO_RATE * 2 / bufferSize + 1];
+            for (int i = 0; i < mSamples.length; i++) {
+                mSamples[i] = ShortBuffer.allocate(bufferSize);
             }
-
-            Log.d(LOG_TAG, "audioRecord.startRecording()");
             mAudioRecord.startRecording();
 
-            /* ffmpeg_audio encoding loop */
+            ShortBuffer audioData;
+            int bufferReadResult;
             while (mRunAudioThread) {
-                if (RECORD_LENGTH > 0) {
-                    audioData = mSamples[mSamplesIndex++ % mSamples.length];
-                    audioData.position(0).limit(0);
-                }
-                //Log.v(LOG_TAG,"recording? " + recording);
+                audioData = mSamples[mSamplesIndex % mSamples.length];
+                audioData.position(0).limit(0);
+
+                mSamplesIndex += 1;
+
                 bufferReadResult = mAudioRecord.read(audioData.array(), 0, audioData.capacity());
                 audioData.limit(bufferReadResult);
                 if (bufferReadResult > 0) {
-                    Log.v(LOG_TAG,"bufferReadResult: " + bufferReadResult);
-                    // If "recording" isn't true when start this thread, it never get's set according to this if statement...!!!
-                    // Why?  Good question...
                     if (mIsRecording) {
-                        if (RECORD_LENGTH <= 0) try {
+                        try {
                             mRecorder.recordSamples(audioData);
-                            //Log.v(LOG_TAG,"recording " + 1024*i + " to " + 1024*i+1024);
                         } catch (FFmpegFrameRecorder.Exception e) {
-                            Log.v(LOG_TAG,e.getMessage());
-                            e.printStackTrace();
+                            Log.v(LOG_TAG, e.getMessage());
                         }
                     }
                 }
             }
-            Log.v(LOG_TAG,"AudioThread Finished, release audioRecord");
+            Log.v(LOG_TAG, "AudioThread Finished, release audioRecord");
 
-            /* encoding finish, release recorder */
             if (mAudioRecord != null) {
                 mAudioRecord.stop();
                 mAudioRecord.release();
                 mAudioRecord = null;
-                Log.v(LOG_TAG,"audioRecord released");
             }
         }
     }
 
-    //---------------------------------------------
-    // camera thread, gets and encodes video data
-    //---------------------------------------------
+    /**
+     * カメラのプレビューを表示するクラス
+     */
     class CameraView extends SurfaceView implements SurfaceHolder.Callback, PreviewCallback {
         private Camera mCamera;
 
@@ -322,6 +269,10 @@ public class MainActivity extends Activity {
 
         @Override
         public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+            if (mCamera == null) {
+                return;
+            }
+
             mCamera.stopPreview();
 
             // 最適なプレビューのサイズを探す
@@ -363,7 +314,7 @@ public class MainActivity extends Activity {
                 mCamera.setPreviewDisplay(holder);
                 mCamera.startPreview();
             } catch (Exception e) {
-                Log.e(LOG_TAG, e.toString());
+                Log.e(LOG_TAG, e.getMessage());
             }
         }
 
@@ -375,7 +326,7 @@ public class MainActivity extends Activity {
                 mCamera.release();
                 mCamera = null;
             } catch (RuntimeException e) {
-                Log.e(LOG_TAG, e.toString());
+                Log.e(LOG_TAG, e.getMessage());
             }
         }
 
@@ -386,27 +337,22 @@ public class MainActivity extends Activity {
                 return;
             }
 
-            Frame yuvImage = null;
-            if (RECORD_LENGTH > 0) {
-                int i = mImagesIndex++ % mImageFrames.length;
-                yuvImage = mImageFrames[i];
-                mTimestamps[i] = DateUtils.SECOND_IN_MILLIS * (System.currentTimeMillis() - mStartTime);
-            }
+            int i = mImagesIndex % mImageFrames.length;
+            mImagesIndex += 1;
+            Frame yuvImage = mImageFrames[i];
 
-            /* get video data */
+            mTimestamps[i] = DateUtils.SECOND_IN_MILLIS * (System.currentTimeMillis() - mStartTime);
             if (yuvImage != null && mIsRecording) {
-                ((ByteBuffer) yuvImage.image[0].position(0)).put(data);
-
-                if (RECORD_LENGTH <= 0) try {
-                    Log.v(LOG_TAG,"Writing Frame");
+                ((ByteBuffer)yuvImage.image[0].position(0)).put(data);
+                try {
+                    Log.d(LOG_TAG, "Writing Frame...");
                     long t = DateUtils.SECOND_IN_MILLIS * (System.currentTimeMillis() - mStartTime);
                     if (t > mRecorder.getTimestamp()) {
                         mRecorder.setTimestamp(t);
                     }
                     mRecorder.record(yuvImage);
                 } catch (FFmpegFrameRecorder.Exception e) {
-                    Log.v(LOG_TAG,e.getMessage());
-                    e.printStackTrace();
+                    Log.v(LOG_TAG, e.getMessage());
                 }
             }
         }
